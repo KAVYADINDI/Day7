a3a7c153b787e82f8d2b354361e1d01f272ee69f
KAKALIDI <KAKALIDI@IN-MUM-LDVM028.corp.capgemini.com>
2018-09-05 09:34:09 -0400

    last commit

--- src/cg/day7/que14/FileOperartionsTest.java
+++ src/cg/day7/que14/FileOperartionsTest.java
@@ -8,6 +8,10 @@ class FileOperartionsTest {
 
 	@Test
 	void test() {
+		assertEquals(true, FileOperations.operations("C:\\Users\\KAKALIDI\\notes\\D4.txt","null",1));
+		assertEquals(true, FileOperations.operations("C:\\Users\\KAKALIDI\\notes\\D4.txt","C:\\Users\\KAKALIDI\\notes\\D2.txt", 2));
+		assertEquals(false, FileOperations.operations("C:\\Users\\KAKALIDI\\notes\\D4.txt","C:\\Users\\KAKALIDI\\notes\\D2.txt", 2));
+		assertEquals(true, FileOperations.operations("C:\\Users\\KAKALIDI\\notes\\D4.txt","null",3));
 
 	}
 
